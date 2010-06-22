4b120a8f68262efe2305ac334f7194166c8bff5a
Ian Molton <ian.molton@collabora.co.uk>
2010-06-22 15:43:08 +0100

    Build fixes

---
+++ target-i386/mesa_mipmap.h
@@ -0,0 +1,4 @@
+extern GLint GLAPIENTRY
+mesa_gluBuild2DMipmaps(GLenum target, GLint components,
+                  GLsizei width, GLsizei height, GLenum format,
+                  GLenum type, const void *data);
